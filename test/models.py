ef0638a642cbe59157bd8587b7562f5bc54c9991
Bohdan Saiko <bohdan.saiko.m@gmail.com>
2026-04-16 12:44:33 +0300

    feat(test): add foreign key to category

--- test/models.py
+++ test/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
+from category.models import Category
 
 class Test(models.Model):
     title = models.CharField(max_length=50)
     create_at = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, related_name='tests') 
 
     def __str__(self):
         return self.title
