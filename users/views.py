70c6173392a6ca2d46e76581aa77b92af5aea329
Bohdan Saiko <bohdan.saiko.m@gmail.com>
2026-04-23 13:26:40 +0300

    feat(users): complete app

--- users/views.py
+++ users/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth import authenticate, login as auth_login
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserLoginForm
 
-def auth(request):
-    form = UserRegisterForm() 
-    return render(request, 'users/auth.html', {'form': form})
+def login_view(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+
+    if request.method != 'POST':
+        form = UserLoginForm()
+        return render(request, 'users/login.html', {'form': form})
+
+    form = UserLoginForm(data=request.POST)
+    
+    if not form.is_valid():
+        return render(request, 'users/login.html', {'form': form})
+
+    user = form.get_user()
+    auth_login(request, user)
+    messages.success(request, f'Вітаємо, {user.username}!')
+    return redirect('home')
 
 def register(request):
     if request.method != 'POST':
@@ -19,10 +34,9 @@ def register(request):
 
     form.save()
     username = form.cleaned_data.get('username')
-    
     messages.success(request, f'Акаунт для {username} успішно створено!')
-    
     return redirect('login')
 
+@login_required
 def profile(request):
     return render(request, 'users/profile.html')
