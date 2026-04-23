31ef9e6138824ba4c646ecaf84ff165537ff483a
Bohdan Saiko <bohdan.saiko.m@gmail.com>
2026-04-23 13:49:37 +0300

    refactor(category): remove create method from public views

--- category/urls.py
+++ category/urls.py
@@ -3,5 +3,4 @@ from .views import category_create, category_list
 
 urlpatterns = [
     path('', category_list, name='category_list'),
-    path('add/', category_create, name='category_create'),
 ]
--- category/views.py
+++ category/views.py
@@ -5,15 +5,3 @@ from .forms import CategoryForm
 def category_list(request):
     categories = Category.objects.all()
     return render(request, 'category/list.html', {'categories': categories})
-
-def category_create(request):
-    if request.method != 'POST':
-        form = CategoryForm()
-        return render(request, 'category/form.html', {'form': form})
-
-    form = CategoryForm(request.POST)
-    if not form.is_valid():
-        return render(request, 'category/form.html', {'form': form})
-
-    form.save()
-    return redirect('category_list')
