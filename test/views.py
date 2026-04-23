4949e4897c71149060baccd3c43c3e132a441510
Bohdan Saiko <bohdan.saiko.m@gmail.com>
2026-04-23 13:38:36 +0300

    feat(category): add category form layer

--- test/views.py
+++ test/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from .models import Test
 from .forms import TestFrom
 
@@ -6,18 +7,22 @@ def test_view(request):
     tests = Test.objects.all()
     return render(request, "test/index.html", { "tests": tests })
 
+@login_required
 def create_test_view(request):
-    if request.method != 'POST':
+    if request.method == 'POST':
+        form = TestFrom(request.POST)
+        if form.is_valid():
+            test_item = form.save(commit=False)
+            
+            test_item.author = request.user
+            
+            test_item.save()
+            
+            return redirect("home") 
+    else:
         form = TestFrom()
-        return render(request, "test/create-test.html", { "form": form })
-    
-    form = TestFrom(request.POST)
-
-    if not form.is_valid:
-        return render(request, "test/create-test.html", { "form": form })
-
-    form.save()
-    return redirect("")
+        
+    return render(request, "test/create-test.html", {"form": form})
 
 
     
